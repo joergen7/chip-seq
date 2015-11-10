6b9514975204d64c9d59adfcc39d187478c4dfb2
Jorgen Brandt <joergen.brandt@onlinehome.de>
2015-11-10 23:28:02 +0100

    Link fixed.

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,7 @@
 #
 # Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
 
-sra_link = "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX%2FSRX243%2FSRX243760"
+sra_link = "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX/SRX243/SRX243760/SRR748312/SRR748312.sra"
 sra_dir  = "#{node.dir.data}/sra"
 sra_file = "#{sra_dir}/SRR748312.sra"
 
