14249b713fd6cf6db8f7451e5a4c7e47efa01a5e
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-11-16 10:09:04 +0100

    Control group added.

--- recipes/data.rb
+++ recipes/data.rb
@@ -5,9 +5,8 @@
 #
 # Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
 
-sra_link = "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX/SRX243/SRX243760/SRR748312/SRR748312.sra"
+id_list = [748312, 748318]
 sra_dir  = "#{node.dir.data}/sra"
-sra_file = "#{sra_dir}/SRR748312.sra"
 
 
 # create directories
@@ -15,9 +14,15 @@ directory node.dir.data
 directory sra_dir
 
 # download read set
-remote_file sra_file do
-  action :create_if_missing
-  source sra_link
-  retries 1
-end
+id_list.each { |id|
+
+  url = "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX/SRX243/SRX243766/SRR#{id}/SRR#{id}.sra"
+  sra_file = "#{sra_dir}/SRR#{id}.sra"
+  
+  remote_file sra_file do
+    action :create_if_missing
+    source url
+    retries 1
+  end
+}
 
