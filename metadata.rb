3cdf789d1f21186c5a1f2f16d572c4c454d6c679
Jorgen Brandt <joergen@cuneiform-lang.org>
2019-03-21 06:41:13 +0100

    Normalization fixed, better Bowtie parameters

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'joergen.brandt@onlinehome.de'
 license          'Apache 2.0'
 description      'Installs/Configures chip-seq'
 long_description 'Installs/Configures chip-seq'
-version          '0.1.0'
+version          '0.1.1'
 chef_version     '>= 12.14' if respond_to?(:chef_version)
 
 depends          'chef-cuneiform', '0.1.4'
--- recipes/data.rb
+++ recipes/data.rb
@@ -20,20 +20,23 @@
 # create directories
 directory node["dir"]["data"]
 
-remote_file "#{node["dir"]["data"]}/SRR576933.sra" do
+remote_file "#{node["dir"]["data"]}/SRR576933" do
   action :create_if_missing
-  source "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByStudy/sra/SRP/SRP015/SRP015911/SRR576933/SRR576933.sra"
+  source "ftp://ftp.sra.ebi.ac.uk/vol1/srr/SRR576/SRR576933"
+  checksum "26fd4d0eea871ee5a4193bc399ba684be5b7b848a1170c20994ea6bc1183be38"
   retries 1
 end
 
-remote_file "#{node["dir"]["data"]}/SRR576938.sra" do
+remote_file "#{node["dir"]["data"]}/SRR576938" do
   action :create_if_missing
-  source "ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByStudy/sra/SRP/SRP015/SRP015911/SRR576938/SRR576938.sra"
+  source "ftp://ftp.sra.ebi.ac.uk/vol1/srr/SRR576/SRR576938"
+  checksum "503e33a4b4b5ae644e2ce48901b7113a416eddbd358832cc5f994e7e1b1ee0d4"
   retries 1
 end
 
 remote_file "#{node["dir"]["data"]}/GCF_000005845.2_ASM584v2_genomic.fna.gz" do
-    action :create_if_missing
-    source "ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz"
-    retries 1
+  action :create_if_missing
+  source "ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz"
+  checksum "a96d3cfa58c88d477013c768f90a11d2386d42a70d566abfb6d9013dcbd24255"
+  retries 1
 end
